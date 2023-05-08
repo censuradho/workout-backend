21779d44f48d2177e9205f375d870b9d41e5a1b7
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-05-08 09:54:56 -0300

    feat(feature module)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { FeaturesModule } from './features';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), FeaturesModule],
   controllers: [AppController],
   providers: [AppService],
 })
