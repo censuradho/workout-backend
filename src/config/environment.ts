6ecc8651147990369defc5fc1114afe3bb55fe2d
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-05-08 09:50:17 -0300

    feat(setup)

---
+++ src/config/environment.ts
@@ -0,0 +1,15 @@
+const env = process.env;
+
+export const environment = {
+  jwtSecret: env.JWT_SECRET,
+  port: env.PORT || 3333,
+  frontendBaseUrl: env.FRONTEND_BASE_URL,
+  nodeMailer: {
+    from: env.NODE_MAIL_FROM,
+    host: env.NODE_MAILER_HOST,
+    auth: {
+      user: env.NODE_MAILER_AUTH_USER,
+      password: env.NODE_MAILER_AUTH_PASS,
+    },
+  },
+};
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { environment } from './config/environment';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  await app.listen(environment.port);
+}
+bootstrap();
